e87b15e5526daac82655166cd24c9118df4d76f2
pd-andy <andrew.thompson@qmul.ac.uk>
2019-12-04 13:19:05 +0000

    :construction: Shuffle around main.js.

--- src/main.js
+++ src/main.js
@@ -4,33 +4,40 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 let startTime = process.hrtime()
-
-// Local Imports ---------------------------------------------------------------
-const { Elm } = require('./Main.elm')
-
-// Elm App ---------------------------------------------------------------------
-const app = Elm.Main.init()
-const send = input => app.ports.fromJs.send({ day, part, year, input })
-
-app.ports.fromElm.subscribe(result => {
-  const endTime = process.hrtime(startTime)
-
-  console.info('Execution time: %ds %dms', endTime[0], endTime[1] / 1000000)
-  console.log(`The result for Advent of Code ${year} day ${day} part ${part} is: ${result}`)
-})
-
-// Get Input -------------------------------------------------------------------
-const { day, part, year } = commandLineArgs([
+let { day, part, year } = commandLineArgs([
   { name: 'day', alias: 'd', type: Number },
   { name: 'part', alias: 'p', type: Number },
   { name: 'year', alias: 'y', type: Number }
 ])
 
+// Elm App ---------------------------------------------------------------------
+const { Elm } = require('./Main.elm')
+const app = Elm.Main.init()
+
+app.solve = input => app.ports.fromJs.send({ day, part, year, input })
+app.ports.fromElm.subscribe(solution => {
+  const endTime = process.hrtime(startTime)
+
+  switch (solution.status) {
+    case 'Ok':
+      console.log(`The result for Advent of Code ${year} day ${day} part ${part} is:\n  ${solution.result}`)
+      break
+    case 'Error':
+      console.log(`Error while solving Advent of Code ${year} day ${day} part ${part}:\n  ${solution.notice}`)
+      break
+  }
+
+  console.info('\nExecution time: %ds %dms', endTime[0], endTime[1] / 1000000)
+})
+
+// Get Input Locally -----------------------------------------------------------
 try {
   const input = require(`../data/${year}/${day}.json`)
 
   startTime = process.hrtime()
-  send(input)
+  app.solve(input)
+
+// Fetch input from server -----------------------------------------------------
 } catch (e) {
   const session = require('../data/session.json')
   const requestParams = {
@@ -46,7 +53,7 @@ try {
       })
 
       startTime = process.hrtime()
-      send(input)
+      app.solve(input)
     })
     .catch(console.log)
 }
